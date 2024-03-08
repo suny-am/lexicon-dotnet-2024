71d5cceb6efae420411248f5f4291d4e44f49e9a
suny-am <visualarea.1@gmail.com>
2024-03-08 18:45:14 +0100

    updated .gitignore

--- main-menu/main-menu/obj/Debug/net8.0/main-menu.AssemblyInfo.cs
+++ main-menu/main-menu/obj/Debug/net8.0/main-menu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main-menu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18156506d33c8e8b3ed6efe882a07af27610d1e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d145ddf86990b7304b5b848f0a66ce1614c7f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("main-menu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main-menu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
