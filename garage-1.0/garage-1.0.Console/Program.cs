39bada69d2e15d41f6f3c803c5799616221baf73
suny-am <visualarea.1@gmail.com>
2024-03-19 20:13:20 +0100

    Implement basic GarageRepository functionality

--- garage-1.0/garage-1.0.Console/Program.cs
+++ garage-1.0/garage-1.0.Console/Program.cs
@@ -1,6 +1,17 @@
 ﻿using Garage_1_0.Library.Models;
+using Garage_1_0.Library.Models.Vehicles;
 using Garage_1_0.Library.Repositories;
 
 Garage<ParkingSpot> garage = new(vehicleCapacity: 30);
 
 GarageRepository<ParkingSpot> garageRepository = new(garage);
+
+Car car1 = new(Guid.NewGuid(), Guid.NewGuid(), "myCar");
+
+garageRepository.Add(car1);
+
+var targetCar = garageRepository.All()?.Single(v => v?.Id == car1.Id);
+
+Console.WriteLine(targetCar?.Name);
+
+
